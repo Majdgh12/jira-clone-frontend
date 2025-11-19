2234b156b7489b7a8baafcc5cd579e0b5cd34c16
majd <majd@example.com>
2025-11-20 01:42:13 +0200

    fix: install astro react integration2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless'; // <-- ADD THIS
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(), // <-- REQUIRED FOR SSR ON VERCEL
   integrations: [react()],
 });
