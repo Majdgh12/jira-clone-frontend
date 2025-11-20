15910cefea41d274114f8ab7f52bd960e76a2e97
majd <majd@example.com>
2025-11-20 15:25:12 +0200

    initial commi1t

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless'; // <-- ADD THIS
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(), // <-- REQUIRED FOR SSR ON VERCEL
-  integrations: [react()],
+  output: "server",
+  adapter: vercel(),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+  ],
 });
